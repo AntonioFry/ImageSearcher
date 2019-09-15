5b45d6afa4467f90154074e901c9dccf019f2ce6
Antonio Fry <antoniofry2000@gmail.com>
2019-09-15 11:51:50 -0600

    create template for Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,12 @@
 <template>
-  
+  <header id='image-searcher-header'>
+    <h1>ImageSearcher</h1>
+  <header>
 </template>
 
 <script>
 export default {
-  
+  name: 'image-searcher-header'
 }
 </script>
 
