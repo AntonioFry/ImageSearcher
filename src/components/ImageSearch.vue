10e246b7604400364deac3f2f4a0d3351b60c06d
Antonio Fry <antoniofry2000@gmail.com>
2019-09-15 13:24:10 -0600

    setup template for SearchImage component

--- src/components/ImageSearch.vue
+++ src/components/ImageSearch.vue
@@ -1,10 +1,14 @@
 <template>
-  
+  <form id="image-search">
+    <label>Search Image</label>
+    <input type="text"/>
+    <button>Search<button>
+  </form>
 </template>
 
 <script>
 export default {
-  
+  name: 'image-search'
 }
 </script>
 
