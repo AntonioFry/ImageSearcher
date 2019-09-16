db5133c57f22f1f35d6c4845006d67fea251c7a2
Antonio Fry <antoniofry2000@gmail.com>
2019-09-15 22:41:27 -0600

    style ImageSearcher coomponent

--- src/components/ImageSearch.vue
+++ src/components/ImageSearch.vue
@@ -1,9 +1,11 @@
 <template>
-  <form id="image-search" @submit.prevent="handleSubmit">
-    <label>Search Image</label>
-    <input type="text"/>
-    <button>Search</button>
-  </form>
+  <section id="image-search">
+    <form @submit.prevent="handleSubmit">
+      <label>Search Image</label>
+      <input type="text"/>
+      <button>Search</button>
+    </form>
+  </section>
 </template>
 
 <script>
@@ -24,6 +26,25 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
-
+<style scoped>
+  section {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    width: 25%;
+  }
+  label {
+    font-size: 1.5em;
+    margin: 10px 0 5px;
+  }
+  button,
+  input {
+    font-size: 1em;
+    height: 20px;
+    margin: 5px 0;
+  }
 </style>
